d65ea767efad9e937585e0eb8094065f11861e58
Pratham Yadav <iiprathamyadav@gmail.com>
2026-07-20 00:48:47 +0530

    fix(build): add capture.html to vite build inputs

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,14 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
-    minify: 'terser'
+    minify: 'terser',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        capture: path.resolve(__dirname, 'capture.html'),
+        renderer: path.resolve(__dirname, 'src/renderer/index.html')
+      }
+    }
   },
   resolve: {
     alias: [
