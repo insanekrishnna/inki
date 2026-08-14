f18992474ff04b4b72fe1cc59391a1331f675e8e
Pratham Yadav <iiprathamyadav@gmail.com>
2026-08-14 20:20:31 +0530

    refactor: remove unnecessary header from Skiper39 component

--- src/components/ui/skiper-ui/skiper39.tsx
+++ src/components/ui/skiper-ui/skiper39.tsx
@@ -285,11 +285,6 @@ const CrowdCanvas = ({ src, rows = 15, cols = 7 }: CrowdCanvasProps) => {
 const Skiper39 = () => {
   return (
     <div className="relative h-full w-full bg-white text-black">
-      <div className="top-22 absolute left-1/2 grid -translate-x-1/2 content-start justify-items-center gap-6 text-center text-black">
-        <span className="relative max-w-[12ch] text-xs uppercase leading-tight opacity-40 after:absolute after:left-1/2 after:top-full after:h-16 after:w-px after:bg-gradient-to-b after:from-white after:to-black after:content-['']">
-          Croud Canvas
-        </span>
-      </div>
       <div className="absolute bottom-0 h-full w-screen">
         <CrowdCanvas src="/images/peeps/all-peeps.png" rows={15} cols={7} />
       </div>
